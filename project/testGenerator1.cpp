a4f16e9b40803c46b02596e27187ed8bedbeb2fc
vmsreeram <112001051@smail.iitpkd.ac.in>
2022-11-26 18:42:07 +0530

    updated makefile with make test that runs all test cases

--- project/testGenerator1.cpp
+++ project/testGenerator1.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-char alphabet[] = {'a', 'b', 'c'};
-int N_LETTERS = 3;
+char alphabet[] = {'a', 'b', 'c', 'd'};
+int N_LETTERS = 4;
 
 std::vector<std::string> get_all_words(int length)
 {
@@ -32,14 +32,14 @@ std::vector<std::string> get_all_words(int length)
 int main(int argc, char* argv[])
 {
     vector<string> vs;
-    for(int len=1;len<=7;len++)
+    for(int len=1;len<=6;len++)
         for(auto s:get_all_words(len))
             vs.push_back(s);
 
     cout << vs.size() <<'\n';
     for(auto s:vs)
     {
-        if(argc == 1)cout << "union(concat(star(symbol(b)),symbol(a)),union(star(symbol(a)),symbol(b)))" << '\n'<<s<<'\n';
+        if(argc == 1)cout << "union(concat(star(symbol(c)),symbol(a)),union(star(symbol(a)),symbol(c)))" << '\n'<<s<<'\n';
         else cout << argv[1] << '\n'<<s<<'\n';
     }
 }
